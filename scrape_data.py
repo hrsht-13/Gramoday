cef23a218901eb75ff56f847979d2d2972113160
Harshita Gupta <hrsht1998@gmail.com>
2021-05-03 01:40:25 +0530

    Update scrape_data.py

--- scrape_data.py
+++ scrape_data.py
@@ -1,11 +1,13 @@
 from kora.selenium import wd
+import pandas pd
+from tdqm import tqdm
 
 market=['Achnera', 'Agra', 'Fatehabad','Fatehpur+Sikri', 'Jagnair', 'Jarar', 'Khairagarh','Samsabad']
 code=['2551','314','2550','2555','2553','2554','2552','2556']
 
 df=pd.DataFrame()
 market=['Achnera', 'Agra', 'Fatehabad','Fatehpur Sikri', 'Jagnair', 'Jarar', 'Khairagarh','Samsabad']
-for i in range(len(market)):
+for i in tqdm(range(len(market))):
   print(i)
   url=("https://agmarknet.gov.in/SearchCmmMkt.aspx?Tx_Commodity=24&Tx_State=UP&Tx_District=1&Tx_Market="+str(code[i])+"&DateFrom=01-Jan-2020&DateTo=31-Dec-2020&Fr_Date=01-Jan-2020&To_Date=31-Dec-2020&Tx_Trend=0&Tx_CommodityHead=Potato&Tx_StateHead=Uttar+Pradesh&Tx_DistrictHead=Agra&Tx_MarketHead="+str(market[i]))
   wd.get(url)
